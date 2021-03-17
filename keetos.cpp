8bf526018ebff386355e20ef04fcc70270a49aa6
kaiwulf <eigenspaces@gmail.com>
2021-03-17 15:59:28 -0400

    simple system to keep track of work

---
+++ keetos.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+Keetos::keetos(vector<string> s) {
+    run(s);
+}
+
+void Keetos::run(vector<string> t) {
+
+}
+
+void Keetos::to_dos() {
+
+}
---
+++ keetos.h
@@ -0,0 +1,18 @@
+#ifndef __KEETOS_
+#define __KEETOS_
+
+// KEEp Track Of Stuff (KEETOS)
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Keetos {
+    keetos(vector<string> s);
+    void run(vector<string> t);
+    void to_dos();
+
+}
+
+#endif
