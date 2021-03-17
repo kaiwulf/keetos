80bf28aef717aeaae1aff6c9488dbff7cceb2f87
kaiwulf <eigenspaces@gmail.com>
2021-03-17 15:58:53 -0400

    a simple user interface

--- ui.cpp
+++ ui.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 #include "ui.h"
+#include "tickets.h"
+
 
 UI::ui()  {
 
@@ -10,6 +12,42 @@ void UI::cli() {
 
 }
 
+void UI::get_args(int argc, char **argv) {
+
+    if(argc > 4) {
+        s_usr_args.assign(argv + 1, argv + argc);
+        arg_parse();
+    } if(argc == 1) {
+        new_ticket();
+    }
+    else {
+        help();
+    }
+}
+
 void UI::arg_parse() {
     
+}
+
+Ticket UI::new_ticket() {
+    string date, start_date, project;
+
+    cout << "Project: " << endl;
+    cin >> project;
+    cout << "Start date: " << endl;
+    cin >> start_date;
+
+
+    Ticket a_new_one(start_date, project);
+    return a_new_one;
+}
+
+Ticket UI::new_ticket(string start_date, string project) {
+    Ticket a_new_one(start_date, project);
+    return a_new_one;
+}
+
+void UI::help() {
+    cout << "Usage:" << endl
+        << "tickets [<project name> <start_date>]" << endl;
 }
--- ui.h
+++ ui.h
@@ -1,11 +1,23 @@
-#include <iostream>
+#ifndef __UI_
+#define __UI_
+#include <string>
+#include <vector>
+#include "ticket.h"
+
+using namespace std;
 
 class UI {
     public:
         ui();
         void cli();
+        void get_args(int argc, char **argv);
         void arg_parse();
+        Ticket new_ticket();
+        Ticket new_ticket(string start_date, string project);
+        void help();
 
     private:
-
+        vector<string> s_usr_args;
 }
+
+#endif
