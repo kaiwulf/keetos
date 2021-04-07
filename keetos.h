22a76965d1bde8799680e7239d3073d62431def6
kaiwulf <eigenspaces@gmail.com>
2021-04-07 15:46:12 -0400

    New vector for implementing reading of xml

--- keetos.h
+++ keetos.h
@@ -6,6 +6,7 @@
 #include <string>
 #include <vector>
 #include <utility>
+#include <fstream>
 
 #include "ticket.h"
 
@@ -26,6 +27,7 @@ class Keetos {
         void to_dos();
         string get_cmd();
     private:
+        // private functions
         void inst_ticket(string start_date, string project_name);
         void init_serialize();
         void push_vec_elems();
@@ -34,6 +36,7 @@ class Keetos {
         string xmlize_line(string bracketed, string center);
         pair<string, string> parse_xml_line(string xml);
 
+        // private inline functions
         inline string bool_to_str(bool b) { string str = b == 0 ? "0" : "1"; return str; }
         inline bool str_to_bool(string s) { bool b00 = s == "0" ? 0 : 1; return b00; }
 
@@ -43,6 +46,8 @@ class Keetos {
         string k_keetos_file;
         string k_str_break;
         vector<Ticket> k_tickets_vec;
+        ofstream xml_out;
+        ifstream xml_in;
 
         // for XML parsing
         string k_header;
@@ -54,6 +59,7 @@ class Keetos {
         string k_body;
         bool k_read;
         vector<string> k_xml_vec;
+        vector<string> k_vec_xml_tags;
 };
 
 #endif
