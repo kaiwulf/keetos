5341e7a0e851a1d34ae3615a55ccd4f1f68b061c
kaism <eigenspaces@gmail.com>
2021-04-08 14:33:44 -0400

    working on args

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <string.h>
 
 #include "ui.h"
 
@@ -7,8 +8,9 @@ using namespace std;
 
 int main(int argc, char **argv) {
     UI ui;
-    if(argv[1] == "?")
-        ui.cli_repl(argc, argv);
-    else
+    // if(strcoll( argv[1], "-l" ) == 0 || strcoll( argv[1], "--loop" ) == 0  )
+    // if(argv[1] == "-l" || argv[1] == "--loop")
+    //     ui.cli_repl(argc, argv);
+    // else
         ui.get_args(argc, argv);
 }
